1f84b2ec225cb461a776322f2d8b533fb4f5f4e3
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-06 16:37:30 -0500

    middle of part5

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('catalog/', include('catalog.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
