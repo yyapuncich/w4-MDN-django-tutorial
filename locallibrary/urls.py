3504aa08c400d95e84bea207f40bbb319b1b6be3
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-07 22:47:35 -0500

    middle part 8 authentication

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-urlpatterns += [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-    path('catalog/', include('catalog.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
